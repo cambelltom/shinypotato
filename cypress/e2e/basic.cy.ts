67cb87eeda9d1c7d6a3cb2e9d9701ef0e0432ac6
Tom Evon <cambelltom@yahoo.com>
2022-06-07 01:47:16 +0200

    Update basic.cy.ts
    Update google test

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,6 +1,6 @@
 it('google test', function(){
     cy.visit('https://www.google.com')
-    cy.get('#L2AGLb > .QS5gu').click()
+    //cy.get('#L2AGLb > .QS5gu').click()
     
     cy.get('.gLFyf')
         .type('automation step by step')
